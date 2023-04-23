b00c527c72ec767e9f022e7335f143ee7aeb3422
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-04-23 17:06:11 +0100

    add dashboard component

---
+++ Frontend/src/Components/Home/Dashboard.jsx
@@ -0,0 +1,36 @@
+import '../../styles/Dashboard.css';
+import CloudIcon from '../svg/cloud';
+import PlusIcon from '../svg/plus';
+import Widgets from './Widgets';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="top">
+        <p className="welcome_text">
+          Hi, <span>Eunice J</span>
+        </p>
+        <div className="links">
+          <div className="link">
+            <CloudIcon />
+            <span>fund wallet</span>
+          </div>
+          <div className="link">
+            <PlusIcon />
+            <span>withdraw</span>
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="left">
+          <Widgets />
+        </div>
+        <div className="right">
+          <p>Notificatons</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
