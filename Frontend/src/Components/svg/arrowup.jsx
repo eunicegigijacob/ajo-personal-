367f09af4d0fb6e3f1282fb1894340b9aeb69d74
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-04-23 17:02:52 +0100

    create icon components

---
+++ Frontend/src/Components/svg/arrowup.jsx
@@ -0,0 +1,21 @@
+const ArrowUpIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.99999 15.8333V4.16667M9.99999 4.16667L4.16666 10M9.99999 4.16667L15.8333 10"
+        stroke="#12B76A"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ArrowUpIcon;
---
+++ Frontend/src/Components/svg/chart.jsx
@@ -0,0 +1,42 @@
+const ChartIcon = () => {
+  return (
+    <svg
+      width="115"
+      height="66"
+      viewBox="0 0 115 66"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M114 1C95.6411 2.73299 94.5042 44.2218 76.3333 49C61.3493 52.9402 53.7617 30.5738 38.6667 33C22.2938 35.6316 17.0544 58.9382 1 65H114V1Z"
+        fill="#ECFDF3"
+      />
+      <path
+        d="M114 1C95.6411 2.73299 94.5042 44.2218 76.3333 49C61.3493 52.9402 53.7617 30.5738 38.6667 33C22.2938 35.6316 17.0544 58.9382 1 65H114V1Z"
+        fill="url(#paint0_linear_118_8935)"
+      />
+      <path
+        d="M1 65C17.0544 58.9382 22.2938 35.6316 38.6667 33C53.7617 30.5738 61.3493 52.9402 76.3333 49C94.5042 44.2218 95.6411 2.73299 114 1"
+        stroke="#12B76A"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_118_8935"
+          x1="57.5"
+          y1="1"
+          x2="57.5"
+          y2="65"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.641167" stop-color="white" stop-opacity="0" />
+          <stop offset="1" stop-color="white" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default ChartIcon;
