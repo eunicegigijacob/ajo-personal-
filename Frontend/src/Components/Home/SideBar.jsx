7c3962f5ee83106d53abd539a758b550eac577de
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-04-23 17:05:31 +0100

    Create sidebar component

---
+++ Frontend/src/Components/Home/SideBar.jsx
@@ -0,0 +1,39 @@
+import '../../styles/Sidebar.css';
+
+import logo from '../../assets/logo.png';
+import Dasboardsvg from '../svg/dashboard';
+import Walletsvg from '../svg/wallet';
+import Savingsvg from '../svg/savings';
+import Feedsvg from '../svg/tips';
+
+const SideBar = () => {
+  return (
+    <div className="Sidebar">
+      <div className="logo">
+        <img src={logo} alt="logo" className="homelogo" />
+      </div>
+      <div className="items">
+        <ul className="list">
+          <li className="item">
+            <Dasboardsvg />
+            <span className="text">Dasboard</span>
+          </li>
+          <li className="item">
+            <Walletsvg />
+            <span className="text"> Wallet </span>
+          </li>
+          <li className="item">
+            <Savingsvg />
+            <span className="text">Savings</span>
+          </li>
+          <li className="item">
+            <Feedsvg />
+            <span className="text">Feed</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
