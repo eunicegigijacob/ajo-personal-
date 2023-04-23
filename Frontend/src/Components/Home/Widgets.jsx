023921c808ce8d67683fa31b1ae5fdc67cc0127b
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-04-23 17:06:23 +0100

    Create Widgets.jsx

---
+++ Frontend/src/Components/Home/Widgets.jsx
@@ -0,0 +1,31 @@
+import '../../styles/Widgets.css';
+import ArrowUpIcon from '../svg/arrowup';
+import ChartIcon from '../svg/chart';
+
+const Widgets = () => {
+  return (
+    <div className="widgets">
+      <div className="widget">
+        <div className="left">
+          <span className="title">Total Amount</span>
+          <span className="Number">$12310</span>
+          <div className="review">
+            <span className="small_text">
+              <ArrowUpIcon />
+              12%
+            </span>
+            <span>vs last month</span>
+          </div>
+        </div>
+        <div className="right">
+          <ChartIcon />
+        </div>
+      </div>
+      <div className="widget">
+        <p>first</p>
+      </div>
+    </div>
+  );
+};
+
+export default Widgets;
