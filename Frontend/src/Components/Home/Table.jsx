48758f22858126e4044ac42422444505b1d64740
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-04-24 17:24:25 +0100

    add table using mui and create table data

--- Frontend/src/Components/Home/Table.jsx
+++ Frontend/src/Components/Home/Table.jsx
@@ -1,7 +1,47 @@
 import styles from '../../styles/Table.module.css';
+import { rows } from '../../assets/Dummy data/TableData';
 
-const Table = () => {
-  return <div className="table">Table</div>;
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const TableComponent = () => {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: '100% ' }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Transaction ID</TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell>Amount</TableCell>
+            <TableCell>Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.TransactionID}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.TransactionID}
+              </TableCell>
+              <TableCell>{row.Date}</TableCell>
+              <TableCell>{row.Type}</TableCell>
+              <TableCell>{row.Amount}</TableCell>
+              <TableCell>{row.Status}</TableCell>
+              <TableCell>View</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
-export default Table;
+export default TableComponent;
---
+++ Frontend/src/assets/Dummy data/TableData.js
@@ -0,0 +1,58 @@
+export const rows = [
+  {
+    TransactionID: 123434,
+    Date: 'Feb 13, 2023',
+    Type: 'Withdrawal',
+    Amount: '$2,329',
+    Status: 'Failed',
+  },
+  {
+    TransactionID: 123434,
+    Date: 'Feb 13, 2023',
+    Type: 'Withdrawal',
+    Amount: '$2,329',
+    Status: 'Failed',
+  },
+  {
+    TransactionID: 123434,
+    Date: 'Feb 13, 2023',
+    Type: 'Withdrawal',
+    Amount: '$2,329',
+    Status: 'Failed',
+  },
+  {
+    TransactionID: 123434,
+    Date: 'Feb 13, 2023',
+    Type: 'Withdrawal',
+    Amount: '$2,329',
+    Status: 'Success',
+  },
+  {
+    TransactionID: 123434,
+    Date: 'Feb 13, 2023',
+    Type: 'Withdrawal',
+    Amount: '$2,329',
+    Status: 'Failed',
+  },
+  {
+    TransactionID: 123434,
+    Date: 'Feb 13, 2023',
+    Type: 'Withdrawal',
+    Amount: '$2,329',
+    Status: 'Success',
+  },
+  {
+    TransactionID: 123434,
+    Date: 'Feb 13, 2023',
+    Type: 'Withdrawal',
+    Amount: '$2,329',
+    Status: 'Pending',
+  },
+  {
+    TransactionID: 123434,
+    Date: 'Feb 13, 2023',
+    Type: 'Withdrawal',
+    Amount: '$2,329',
+    Status: 'Failed',
+  },
+];
