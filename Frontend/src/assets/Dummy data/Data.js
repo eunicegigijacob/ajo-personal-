6498814657a131d5f0d32bfef39a1c3f73ae5614
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-04-24 13:59:25 +0100

    Create dummy data for bar chart

---
+++ Frontend/src/assets/Dummy data/Data.js
@@ -0,0 +1,50 @@
+export const SavingsData = [
+  {
+    Month: 'Jan',
+    Amount: 800,
+  },
+  {
+    Month: 'Feb',
+    Amount: 1000,
+  },
+  {
+    Month: 'Mar',
+    Amount: 600,
+  },
+  {
+    Month: 'Apr',
+    Amount: 900,
+  },
+  {
+    Month: 'May',
+    Amount: 600,
+  },
+  {
+    Month: 'Jun',
+    Amount: 950,
+  },
+  {
+    Month: 'Jul',
+    Amount: 850,
+  },
+  {
+    Month: 'Aug',
+    Amount: 900,
+  },
+  {
+    Month: 'Sep',
+    Amount: 880,
+  },
+  {
+    Month: 'Oct',
+    Amount: 1000,
+  },
+  {
+    Month: 'Nov',
+    Amount: 1500,
+  },
+  {
+    Month: 'Dec',
+    Amount: 800,
+  },
+];
