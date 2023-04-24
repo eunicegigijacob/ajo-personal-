3b1a8055366cf57a0a21a75fa90af8fb25d05d93
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-04-24 14:00:32 +0100

    import components from recharts library to create bar chart

--- Frontend/src/Components/Home/Barchart.jsx
+++ Frontend/src/Components/Home/Barchart.jsx
@@ -1,9 +1,34 @@
 import styles from '../../styles/Barchart.module.css';
 
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  XAxis,
+  Tooltip,
+  CartesianGrid,
+  Legend,
+  YAxis,
+} from 'recharts';
+import { SavingsData } from '../../assets/Dummy data/Data.js';
+
 const Barchart = () => {
   return (
-    <div className="barchart">
-      <p>Heloooo</p>
+    <div className={styles.barchart}>
+      <div className={styles.barchartwrapper}>
+        <ResponsiveContainer width="100%">
+          <BarChart
+            data={SavingsData}
+            margin={{ top: 10, left: 20, right: 20, bottom: 10 }}
+          >
+            {/* <CartesianGrid strokeDasharray="3 3" /> */}
+            <XAxis dataKey="Month" stroke="#2A5C99" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="Amount" stroke="#2A5C99" fill="#2A5C99" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
