21bcb85a971512f85c548d55429d42751b4c97a5
EUNICE JACOB <115852649+eunicegigijacob@users.noreply.github.com>
2023-04-24 12:35:30 +0100

    create barchart component

---
+++ Frontend/src/Components/Home/Barchart.jsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/Barchart.module.css';
+
+const Barchart = () => {
+  return (
+    <div className="barchart">
+      <p>Heloooo</p>
+    </div>
+  );
+};
+
+export default Barchart;
